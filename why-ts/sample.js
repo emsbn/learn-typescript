cfed7c34528937a423aad65171155e9dff8f90be
emsbn <emsbn1@gmail.com>
2021-01-14 00:09:22 +0900

    sample

---
+++ why-ts/sample.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+const result = sum(10, '20');
---
+++ why-ts/sample.ts
@@ -0,0 +1,7 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+// const result = sum(10, 20);
+const result = sum(10, '20');
+result.toLocaleString();
