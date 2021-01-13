94b706b40d6821459ef26c2d1a909264cb655cd0
emsbn <emsbn1@gmail.com>
2021-01-13 23:45:09 +0900

    remove md file

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,10 +1,10 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = "https://jsonplaceholder.typicode.com/users";
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var nameNode = document.querySelector("#username");
+var emailNode = document.querySelector("#email");
+var addressNode = document.querySelector("#address");
 
 // user data
 var user = {};
@@ -13,9 +13,14 @@ function startApp() {
   axios
     .get(url)
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      const { name, email, address } = user[0];
+      nameNode.innerText = name;
+      emailNode.innerText = email;
+      addressNode.innerText = address.street;
     })
     .catch(function (error) {
       console.log(error);
