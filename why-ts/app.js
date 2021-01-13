c3b71f84c3d0a4294823e75f4b8762742c0e222c
emsbn <emsbn1@gmail.com>
2021-01-13 23:55:57 +0900

    jsdoc

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,5 +1,5 @@
 // api url
-var url = "https://jsonplaceholder.typicode.com/users";
+var url = "https://jsonplaceholder.typicode.com/users/1";
 
 // dom
 var nameNode = document.querySelector("#username");
@@ -9,15 +9,40 @@ var addressNode = document.querySelector("#address");
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {
+  response.address;
+});
+
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  fetchUser
     .then(function (response) {
       // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
       console.log(user);
-      const { name, email, address } = user[0];
+      const { name, email, address } = user;
       nameNode.innerText = name;
       emailNode.innerText = email;
       addressNode.innerText = address.street;
