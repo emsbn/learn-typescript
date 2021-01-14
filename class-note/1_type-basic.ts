b1575afc0e61007496c7db0bd25a06b6438f521b
emsbn <emsbn1@gmail.com>
2021-01-14 19:07:34 +0900

    class-note

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,30 @@
+// JS 문자열 선언
+// const str = 'hello';
+
+// TS 문자열 선언
+const str: string = 'hello';
+
+// TS 숫자
+const number: number = 10;
+
+// TS 배열
+const arr: Array<number> = [1, 2, 3];
+const heroes: Array<string> = ['Capt', 'Thor', 'Bruce'];
+const items: number[] = [1, 2, 3];
+
+// TS 튜플
+const address: [string, number] = ['gangnam', 100];
+
+// TS 객체
+const obj: object = {};
+const person: object = {
+  name: 'capt',
+  age: 100,
+};
+const person2: { name: string; age: number } = {
+  name: 'capt',
+  age: 100,
+};
+
+// TS 진위값
+let show: boolean = true;
